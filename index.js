d43bdd2c784464a08019c529b6b19798e5751dd2
Michal Delikat <michal.delikat0@gmail.com>
2022-07-09 15:36:29 +0200

    wszystko_na_jednej_stronie_usunieta_milosc_zdjecia_z_pragi

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ class MyHeader extends HTMLElement {
                     <li><a href="index.html#index">Strona główna</a></li>
                     <li><a href="index.html#o_mnie">O mnie</a></li>
                     <li><a href="index.html#projekty">Projekty</a></li>
-                    <li><a href="index.html#milosc">Miłość</a></li>
                     <li><a href="index.html#kontakt">Kontakt</a></li>
                 </ul>
             </nav>
