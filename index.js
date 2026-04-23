76196668a182f2b7d0f3e16cb906ac2fb1a18ae5
Michał Delikat <michal.delikat0@gmail.com>
2026-04-23 22:16:02 +0200

    Umieszczenie polityki prywatności w akordeonie

--- index.js
+++ index.js
@@ -44,3 +44,14 @@ window.addEventListener("scroll", () => {
         toTop.classList.remove("active")
     }
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const accordionHeader = document.querySelector('.accordion-header');
+    accordionHeader.addEventListener('click', () => {
+        const content = accordionHeader.nextElementSibling;
+        const arrow = accordionHeader.querySelector('.accordion-arrow');
+        
+        content.classList.toggle('show');
+        arrow.classList.toggle('rotate');   
+    });
+});
